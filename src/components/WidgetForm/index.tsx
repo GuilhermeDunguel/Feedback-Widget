dae7e048888b9feb820dd84e0e938c618b99fc1b
Guilherme Dunguel <gdunguel2012@gmail.com>
2022-05-19 19:19:46 -0300

    Second Commit

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -68,7 +68,7 @@ export function WidgetForm() {
          )}
 
          <footer className="text-xs text-neutral-400">
-            Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
+            Desenvolvido com ♥ por <a className="underline underline-offset-2" href="https://guilhermedunguel.com">Guilherme Dunguel</a> para a NLW
          </footer>
       </div>
    )
